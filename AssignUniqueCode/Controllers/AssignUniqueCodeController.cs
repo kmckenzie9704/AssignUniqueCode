608331820f37fbce2f722e108def77d8d114d5c6
Kenneth McKenzieddfsdf <kenneth.mckenzie@myacuity.coms>
2018-03-14 08:22:16 -0400

    Add project files.

---
+++ AssignUniqueCode/Controllers/AssignUniqueCodeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AssignUniqueCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace AssignUniqueCode.Controllers
+{
+    [Route("api/AssignUniqueCode")]
+    public class AssignUniqueCodeController : Controller
+    {
+        private DatabaseContext _context;
+
+        public AssignUniqueCodeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            string strUniqueCode = "Assign Unique Code to applicant.";
+            return strUniqueCode;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public bool Post([FromBody]Applicant applicant)
+        {
+            bool blnUniqueCodeIsValid = AssignCode(applicant.appUniqueCode, applicant.appEmail);
+
+            return blnUniqueCodeIsValid;
+        }
+
+        private bool AssignCode(string strUniqueCode, string strEmail)
+        {
+            bool blnCodeUpdated = false;
+            using (_context)
+            {
+                UniqueCode newCode = new UniqueCode();
+                DateTime datToday = DateTime.Today;
+                newCode.uniCode = strUniqueCode;
+                newCode.uniEmail = strEmail;
+                newCode.uniAssignDate = datToday;
+                _context.UniqueCodes.Add(newCode);
+                _context.SaveChanges();
+                blnCodeUpdated = true;
+            }
+            return blnCodeUpdated;
+        }
+    }
+
+}
---
+++ AssignUniqueCode/Models/Applicant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignUniqueCode.Models
+{
+    public class Applicant
+    {
+        public int appId { get; set; }
+        public string appFirstName { get; set; }
+        public string appLastName { get; set; }
+        public DateTime? appBirthdate { get; set; }
+        public string appStatus { get; set; }
+        public string appUniqueCode { get; set; }
+        public string appResolution { get; set; }
+        public string appFilename { get; set; }
+        public string appEmail { get; set; }
+        public string appImage { get; set; }
+    }
+}
